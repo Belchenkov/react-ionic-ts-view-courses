d7933824c8760105f70326f75a8418a6c346572d
Aleksey Belchenkov <u608110@gmail.com>
2020-08-10 13:40:51 +0300

    Add Global Styles

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 import './theme/variables.css';
+import './theme/theme.css';
 
 import Courses from "./pages/Courses";
 import CourseGoals from "./pages/CourseGoals";
