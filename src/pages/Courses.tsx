428a3412dd766af9b637059a7e6258d00d54ebeb
Aleksey Belchenkov <u608110@gmail.com>
2020-08-13 13:23:02 +0300

    Adding Dummy Data

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -4,10 +4,30 @@ import {
     IonHeader,
     IonTitle,
     IonToolbar,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonCardContent,
+    IonCard,
     IonButton,
     IonPage
 } from "@ionic/react";
 // import { useHistory } from 'react-router-dom';
+
+const COURSE_DATA = [
+    {
+        id: 'c1',
+        title: 'React - The Complete Guide (incl Hooks, React Router, Redux)'
+    },
+    {
+        id: 'c2',
+        title: 'React Front To Back'
+    },
+    {
+        id: 'c3',
+        title: 'Ionic - Build iOS, Android & Web Apps with Ionic'
+    }
+];
 
 const Courses: React.FC = () => {
     /*
@@ -25,12 +45,27 @@ const Courses: React.FC = () => {
                     <IonTitle>Courses</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent>
+            <IonContent class="ion-padding">
                 <h2>Courses Page</h2>
-                <div>
-                    <IonButton routerLink="/course-goals">To Course Goals</IonButton>
-                    {/*<IonButton onClick={changePageHandler}>To Course Goals</IonButton>*/}
-                </div>
+                <IonGrid>
+                    { COURSE_DATA.map(course => (
+                        <IonRow key={course.id}>
+                            <IonCol size-md="4" offset-md="4">
+                                <IonCard>
+                                    <IonCardContent>
+                                        <h2 className="ion-text-center">{ course.title }</h2>
+                                        <div className="ion-text-center ion-margin-top">
+                                            <IonButton
+                                                color="success"
+                                                routerLink=""
+                                            >Details</IonButton>
+                                        </div>
+                                    </IonCardContent>
+                                </IonCard>
+                            </IonCol>
+                        </IonRow>
+                    ))}
+                </IonGrid>
             </IonContent>
         </IonPage>
     );
